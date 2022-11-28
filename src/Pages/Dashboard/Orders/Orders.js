a6b81613a8edd4ee6b606847684c40a89fa29d7e
rokib2000 <rokib0w1@gmail.com>
2022-11-29 05:13:54 +0600

    show my order and order data

--- src/Pages/Dashboard/Orders/Orders.js
+++ src/Pages/Dashboard/Orders/Orders.js
@@ -1,10 +1,26 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import React, { useContext } from "react";
+import { AuthContext } from "../../../contexts/AuthProvider";
+import Loading from "../../Shared/Loading/Loading";
 
 const Orders = () => {
+  const { user, loading } = useContext(AuthContext);
+  const { data: orders = [], isLoading } = useQuery({
+    queryKey: ["products"],
+    queryFn: async () => {
+      const res = await fetch(`http://localhost:5000/orders?sellerEmail=${user.email}`);
+      const data = await res.json();
+      return data;
+    },
+  });
+
+  if (isLoading || loading) {
+    return <Loading></Loading>;
+  }
+
   return (
     <div className="w-[90%] mx-auto">
-      <h1 className="text-4xl font-semibold my-6">Seller</h1>
+      <h1 className="text-4xl font-semibold my-6">Order</h1>
 
       <table className="border-collapse w-full">
         <thead>
@@ -16,51 +32,52 @@ const Orders = () => {
               Product Name
             </th>
             <th className="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell">
-              Category
+              Buyer Name
             </th>
             <th className="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell">
-              Location
+              Buyer Email
             </th>
             <th className="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell">
-              Actions
+              Status
             </th>
           </tr>
         </thead>
         <tbody>
-          <tr className="bg-white lg:hover:bg-gray-100 flex lg:table-row flex-row lg:flex-row flex-wrap lg:flex-no-wrap mb-10 lg:mb-0">
-            <td className="w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static">
-              <div className="avatar">
-                <div className="w-16 rounded-full">
-                  <img src="https://placeimg.com/192/192/people" alt="" />
+          {orders?.map((order) => (
+            <tr className="bg-white lg:hover:bg-gray-100 flex lg:table-row flex-row lg:flex-row flex-wrap lg:flex-no-wrap mb-10 lg:mb-0">
+              <td className="w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static">
+                <div className="avatar">
+                  <div className="w-16 rounded-lg">
+                    <img src={order.image} alt="" />
+                  </div>
                 </div>
-              </div>
-            </td>
-            <td className="w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static">
-              <span className="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">
-                Product Name
-              </span>
-              KnobHome
-            </td>
-            <td className="w-full lg:w-auto p-3 text-gray-800 text-center border border-b  block lg:table-cell relative lg:static">
-              <span className="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">
-                Category
-              </span>
-              German
-            </td>
-            <td className="w-full lg:w-auto p-3 text-gray-800 text-center border border-b  block lg:table-cell relative lg:static">
-              <span className="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">
-                Location
-              </span>
-              Mirpur
-            </td>
-            <td className="w-full lg:w-auto p-3 text-gray-800 text-center border border-b  block lg:table-cell relative lg:static">
-              <span className="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">
-                Actions
-              </span>
-              <Link className="text-blue-400 hover:text-blue-600 underline">Edit</Link>
-              <Link className="text-blue-400 hover:text-blue-600 underline pl-6">Remove</Link>
-            </td>
-          </tr>
+              </td>
+              <td className="w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static">
+                <span className="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">
+                  Product Name
+                </span>
+                {order.title}
+              </td>
+              <td className="w-full lg:w-auto p-3 text-gray-800 text-center border border-b  block lg:table-cell relative lg:static">
+                <span className="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">
+                  Buyer Name
+                </span>
+                {order.buyerName}
+              </td>
+              <td className="w-full lg:w-auto p-3 text-gray-800 text-center border border-b  block lg:table-cell relative lg:static">
+                <span className="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">
+                  Buyer Email
+                </span>
+                {order.buyerEmail}
+              </td>
+              <td className="w-full lg:w-auto p-3 text-gray-800 text-center border border-b  block lg:table-cell relative lg:static">
+                <span className="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">
+                  Status
+                </span>
+                xxxxxxxx
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
